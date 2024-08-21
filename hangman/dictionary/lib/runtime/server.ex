1c94aa8f7eccd9435451813361c3796496c0eb36
Ivan Giovanazzi <ivanog@pm.me>
2024-08-21 01:27:54 -0300

    added gen server to hangman

--- hangman/dictionary/lib/runtime/server.ex
+++ hangman/dictionary/lib/runtime/server.ex
@@ -10,9 +10,6 @@ defmodule Dictionary.Runtime.Server do
     def start_link(_), do: Agent.start_link(&WordList.word_list/0, name: @me)
 
     def random_word do
-        if :rand.uniform < 0.33 do
-            Agent.get(@me, fn _ -> exit(:boom) end)
-        end
         Agent.get(@me, &WordList.random_word/1)
     end
 end
