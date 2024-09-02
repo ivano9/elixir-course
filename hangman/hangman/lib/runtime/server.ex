8eca1c5753f9a397065ad83001da7316ee8f04f9
Ivan Giovanazzi <ivanog@pm.me>
2024-09-02 01:38:02 -0300

    added watchdog to kill zombie process

--- hangman/hangman/lib/runtime/server.ex
+++ hangman/hangman/lib/runtime/server.ex
@@ -1,6 +1,10 @@
 defmodule Hangman.Runtime.Server do
     @type t :: pid
+
+    @idle_timeout 1 * 60 * 60 * 1000   # 1 hour
+
     alias Hangman.Impl.Game
+    alias Hangman.Runtime.Watchdog
 
     use GenServer
 
@@ -11,16 +15,19 @@ defmodule Hangman.Runtime.Server do
 
     ### server process
     def init(_) do
+        watcher = Watchdog.start(@idle_timeout)
         :logger.notice("new game begin created")
-        {:ok, Game.new_game}
+        {:ok, {Game.new_game, watcher}}
     end
 
-    def handle_call({:make_move, guess}, _from, game) do
+    def handle_call({:make_move, guess}, _from, {game, watcher}) do
+        Watchdog.im_alive(watcher)
         {update_game, tally} = Game.make_move(game, guess)
-        {:reply, tally, update_game}
+        {:reply, tally, {update_game, watcher}}
     end
 
-    def handle_call({:tally}, _from, game) do
-        {:reply, Game.tally(game), game}
+    def handle_call({:tally}, _from, {game, watcher}) do
+        Watchdog.im_alive(watcher)
+        {:reply, Game.tally(game), {game, watcher}}
     end
 end
