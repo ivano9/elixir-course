bdc9b483e62165e873286d7600435d7df6e14658
Ivan Giovanazzi <ivanog@pm.me>
2024-09-05 01:31:59 -0300

    liveview svg hangman done

---
+++ hangman/b2/lib/b2_web/live/game.ex
@@ -0,0 +1,25 @@
+defmodule B2Web.Live.Game do
+    use Phoenix.LiveView
+
+    def mount(_params, _session, socket) do
+        game = Hangman.new_game()
+        tally = Hangman.tally(game)
+        socket = socket |> assign(%{game: game, tally: tally})
+        {:ok, socket}
+    end
+
+    def handle_event("make_move", %{"key" => key}, socket) do
+        tally = Hangman.make_move(socket.assigns.game, key)
+        {:noreply, assign(socket, :tally, tally)}
+    end
+
+    def render(assigns) do
+        ~H"""
+            <div class="game-holder" phx-window-keyup="make_move">
+                <.live_component module={__MODULE__.Figure} id={1} tally={@tally}/>
+                <.live_component module={__MODULE__.Alphabet} id={2} tally={@tally}/>
+                <.live_component module={__MODULE__.WordSoFar} id={3} tally={@tally}/>
+            </div>
+        """
+    end
+end
